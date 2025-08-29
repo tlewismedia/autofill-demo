8eaeb8fa44348bcfa0e24e7926c992de039d3c88
Tom Lewis <tom.lewis@dharbor.com>
2025-08-29 13:10:08 -0700

    initial poc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const intitialState = {
+  fields: {
+    firstName: {
+      showSuggestions: true,
+      suggestion: 'Tom',
+      suggestionApplied: false,
+      suggestionTransitioned: false,
+      value: '',
+    },
+    lastName: {
+      showSuggestions: true,
+      suggestion: 'Lewis',
+      value: '',
+    },
+  },
+};
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true,
 })
 export class App {
   protected title = 'autofill';
+  state = JSON.parse(JSON.stringify(intitialState));
+
+  suggestionClick(key: string, value: string) {
+    // set hide state for suggestions
+    this.state.fields[key].showSuggestions = false;
+
+    // set value of field to value and fade in
+    this.state.fields[key].value = value;
+    this.state.fields[key].suggestionApplied = true;
+    setTimeout(() => {
+      this.state.fields[key].suggestionTransitioned = true;
+    }, 200);
+  }
+
+  reset() {
+    this.state = JSON.parse(JSON.stringify(intitialState));
+  }
 }
